8092a50d1353594092f4ea3a81c428cf6b2152a1
Catalin Plesu <catalinhimself@gmail.com>
2025-09-13 17:56:59 +0300

    added the base view for the master cv for the heading

--- app/controllers/master_cv_controller.rb
+++ app/controllers/master_cv_controller.rb
@@ -1,5 +1,14 @@
 class MasterCvController < ApplicationController
   def index
+    @full_name = "Catalin Plesu"
+    @heading = [
+      { icon: "mobile", text: "+373 67 655 472", url: "tel:+37367655472" },
+      { icon: "at", text: "catalin.plesu@proton.me", url: "mailto:catalin.plesu@proton.me" },
+      { icon: "square-linkedin", text: "linkedin", url: "https://www.linkedin.com/in/c%c4%83t%c4%83lin-ple%c8%99u-042872209/" },
+      { icon: "Github", text: "github", url: "https://github.com/catalinplesu" },
+      { icon: "globe", text: "site", url: "https://catalinplesu.xyz" },
+      { icon: "map-marker", text: "Chișinău, Moldova", url: nil }
+    ]
   end
 
   def show
