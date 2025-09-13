b27ddbdcfe6a425b9b9f3852d6fc8c046ace5c7e
Catalin Plesu <catalinhimself@gmail.com>
2025-09-13 14:39:58 +0300

    generated home controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
   root to: "home#index"
+  get "home/index"
 end
