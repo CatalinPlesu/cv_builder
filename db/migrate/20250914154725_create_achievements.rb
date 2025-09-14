85c5bd34ffe3294b2d9d49258c4d4a02b43fe039
Catalin Plesu <catalinhimself@gmail.com>
2025-09-14 18:47:49 +0300

    generated achievement model

---
+++ db/migrate/20250914154725_create_achievements.rb
@@ -0,0 +1,13 @@
+class CreateAchievements < ActiveRecord::Migration[8.0]
+  def change
+    create_table :achievements do |t|
+      t.string :title
+      t.text :description
+      t.string :date
+      t.integer :position
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
