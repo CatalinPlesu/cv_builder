905b7603410fca862de7346528939d584f143b03
Catalin Plesu <catalinhimself@gmail.com>
2025-09-16 14:40:39 +0300

    build script with no chache

--- script/build.sh
+++ script/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 		# -e SECRET_KEY_BASE="b5ba8dbad4f0ec1d9f17adb71e96bb9f" \
+# podman build --no-cache -t cv_builder:latest .
 podman build -t cv_builder:latest .
 podman image save cv_builder:latest -o cv_builder.image
